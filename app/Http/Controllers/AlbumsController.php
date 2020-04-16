5657eaa18493d12b162d1bc9ac0526aa3e5635c4
nathan saide <nathansaide@nathans-MacBook-Pro.local>
2020-04-16 16:43:26 +0100

    managed to get the routes done and the album image info stored on the database

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\Album;
 
 class AlbumsController extends Controller
 {
-    //
+    public function index() {
+        return view('albums.index');
+    }
+    public function create() {
+        return view('albums.create');
+    }
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required',
+            'description' => 'required',
+            'cover-image' => 'required|image'
+        ]);
+
+        $filenameWithExtension = $request->file('cover-image')->getClientOriginalName();
+        $filename = pathinfo($filenameWithExtension, PATHINFO_FILENAME);
+        $extension = $request->file('cover-image')->getClientOriginalExtension();
+        $filenameToStore = $filename . '_' . time() . '.' . $extension;
+        $request->file('cover-image')->storeAs('public/album_covers', $filenameToStore);
+        $album = new Album();
+        $album->name = $request->input('name');
+        $album->description = $request->input('description');
+        $album->cover_image = $filenameToStore;
+        $album->save();
+        return redirect('/albums')->with('success', 'Album created successfully');
+    }
 }
